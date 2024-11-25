347790e100a19c515c8e97f8a6517dcfe5eb34ea
NeoooDev <124891218+BrentBenito-Dev@users.noreply.github.com>
2024-11-25 20:19:22 +0800

    fix: added missing files

---
+++ foundations/js/variables_lesson/numbers.js
@@ -0,0 +1,36 @@
+let x;
+let y;
+
+// Addition
+x = 23;
+y = 97;
+
+console.log("Addition: 23 + 97 = " + x + y);
+// Adding 6 sequences of numbers
+console.log("Sum of the first 6 counting numbers: " + 1 + 2 + 3 + 4 + 5 + 6);
+
+// combination
+console.log((4 + 6 + 9)/ 77)
+
+if (1+2 == 3.0){
+    console.log("They are equal");
+}
+
+if (1+2 === 3.0){
+    console.log("They are equal");
+}else{
+    console.log("They have different data types");
+}
+
+y = "WASD";
+
+if (isNaN(x+y)){
+    console.log("one of them is not a number");
+}
+
+// Unary
+
+x = "3";
+y = "2";
+
+alert(+x + +y);
---
+++ foundations/js/variables_lesson/variables.js
@@ -0,0 +1,12 @@
+let firstName = "John"
+let lastName = "Doe"
+const age = 21;
+
+firstName = "Alita"
+
+console.log(firstName)
+console.log(lastName)
+
+console.log("Hello " + firstName + " " + lastName)
+console.log("Your age is " + age)
+
