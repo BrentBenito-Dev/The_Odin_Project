d216512dd91056526c5b6d5984ba3df7e33d5bc4
NeoooDev <124891218+BrentBenito-Dev@users.noreply.github.com>
2024-12-20 19:48:29 +0800

    feat: finished general structure of te webpage

---
+++ foundations/js/problem-solving-lesson/fizzbuzz.js
@@ -0,0 +1,14 @@
+let answer = parseInt(prompt("Please enter the number would you like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++){
+	if (i % 3 === 0 && i % 5 === 0){
+		console.log("FizzBuzz");
+	}else if(i % 3 === 0){
+		console.log("Fizz")
+	}else if( i % 5 ===0){
+		console.log("Buzz");
+	}else{
+		console.log(i);
+	}
+}
+
